bde15d0d429d8342d0f4d4c859646dd2826a3f42
SoftLab <softlab2022@gmail.com>
2022-03-28 16:57:24 +0600

    fix js

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -2,7 +2,10 @@
     $(document).ready(function(){
         
         //Handle contact form
+        if(typeof elementor === 'undefined'){
         $('.softlab-contact-form:not(#pre-sales-form)').hide();
+        }
+
         $('.contact-tab').on('click', function(){
             const id = $(this).attr('id');
             const form = $(`#${id}-form`);
