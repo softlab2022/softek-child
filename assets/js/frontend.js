3615fdf3f0558b1c7be1c2116234ee65ba87cd18
SoftLab <softlab2022@gmail.com>
2022-03-28 16:33:46 +0600

    add contact form handling

---
+++ assets/js/frontend.js
@@ -0,0 +1,18 @@
+;(function($){
+    $(document).ready(function(){
+        
+        //Handle contact form
+        $('.contact-tab').on('click', function(){
+            const id = $(this).attr('id');
+            const form = $(`#${id}-form`);
+
+            $('.contact-tab').removeClass('active');
+            $(this).addClass('active');
+
+            $('.softlab-contact-form').hide();
+            form.show();
+
+            
+        });
+    });
+})(jQuery);
--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ function softek_child_enqueue_styles() {
         array( 'softek-style' ),
         1.0
     );
+
+    wp_enqueue_script('softlab-js',  get_stylesheet_directory_uri() . '/assets/js/frontend.js', ['jquery'], time(), true);
 }
 
 add_action(  'wp_enqueue_scripts', 'softek_child_enqueue_styles', 99 );
